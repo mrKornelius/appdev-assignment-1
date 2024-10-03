09a3b9643871a42e0e6d843aa87096d00acd5fc3
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-03 23:04:58 +0200

    New game physics

--- game/GameScreen.tsx
+++ game/GameScreen.tsx
@@ -3,81 +3,90 @@ import { Subscription } from "expo-sensors/build/Pedometer";
 import { useEffect, useState } from "react";
 import {
   Alert,
+  Dimensions,
   Pressable,
+  SafeAreaView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
 } from "react-native";
+import { area, Coord2d, distance, limitOutput } from "./utils";
 
-type Coord = { x: number; y: number; z: number };
-type Coord2d = { x: number; y: number };
+const { width, height } = Dimensions.get("window");
+const gridSize = Math.min(width / area[0].length, height / area.length);
+const offset = gridSize / 2;
+const startPosition: Coord2d = { x: 100, y: 100 };
+const BOUNCE_DAMPING = 0.95;
+const UPDATE_INTERVAL = 20;
 
-// const list = useMemo(() => {}, [])
-const gridSize = 50;
-const area = [
-  [0, 0, 0, 2, 0, 0, 0, 2],
-  [0, 0, 0, 0, 0, 0, 0, 2],
-  [0, 2, 0, 0, 0, 0, 0, 2],
-  [0, 2, 0, 2, 0, 0, 0, 2],
-  [0, 2, 0, 2, 2, 0, 0, 2],
-  [0, 2, 0, 2, 1, 0, 0, 2],
-  [0, 2, 0, 0, 0, 0, 0, 2],
-  [3, 2, 0, 0, 0, 3, 0, 2],
-];
-const distance = (p: Coord2d, q: Coord2d) =>
-  Math.sqrt(Math.pow(p.x - q.x, 2) + Math.pow(p.y - q.y, 2));
-
+//FIXME: type!
 export default function GameScreen({ navigation }: any) {
-  //FIXME: type!
+  // setup states
   const startPosition: Coord2d = { x: 100, y: 100 };
-  const goal: Coord2d = { x: 100, y: 300 };
-
+  const goal: Coord2d = { x: 100, y: 600 };
   const [position, setPosition] = useState({ ...startPosition });
-  const [{ x, y, z }, setData] = useState<Coord>({ x: 0, y: 0, z: 0 });
-
-  const [subscription, setSubscription] = useState<Subscription | null>(null);
-  const _slow = () => Accelerometer.setUpdateInterval(500);
-  const _fast = () => Accelerometer.setUpdateInterval(16);
-  const _subscribe = () => {
-    setSubscription(Accelerometer.addListener(updatePosition));
-    // console.log("sub:", subscription);
-  };
-
-  const _unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
-  };
+  const [velocity, setVelocity] = useState<Coord2d>({ x: 0, y: 0 });
+  const [time, setTime] = useState(Date.now());
+  const [senorEnabled, setSensorEnabled] = useState(false);
+  const [startTime, setStartTime] = useState(0);
 
   useEffect(() => {
-    _subscribe();
-    return () => _unsubscribe();
-  }, []);
+    let subscription: Subscription;
+    if (senorEnabled) {
+      subscription = Accelerometer.addListener(({ x, y }) => {
+        setVelocity((prev) => ({
+          // limit velocity change to +/-2 and map x to -x
+          x: prev.x - limitOutput(x, -2, 2),
+          y: prev.y + limitOutput(y, -2, 2),
+        }));
+      });
+      Accelerometer.setUpdateInterval(UPDATE_INTERVAL);
+      setStartTime(Date.now());
+    } else {
+      if (Accelerometer.hasListeners()) {
+        Accelerometer.removeAllListeners();
+      }
+    }
+    return () => subscription?.remove();
+  }, [senorEnabled]);
 
-  const toggleSubscription = () =>
-    subscription ? _unsubscribe() : _subscribe();
+  const handleGyroToggle = () => setSensorEnabled(!senorEnabled);
+
+  useEffect(() => {
+    let t = Date.now();
+    let dt = limitOutput(t - time, 0, 2 * UPDATE_INTERVAL);
+    // console.log("new velocity dt=", dt);
+
+    setPosition((prev) => {
+      let newX = prev.x + (velocity.x * dt) / 500;
+      if (newX - 50 < 0 || newX > width - 50)
+        setVelocity({ x: -BOUNCE_DAMPING * velocity.x, y: velocity.y });
+
+      let newY = prev.y + (velocity.y * dt) / 500;
+      if (newY - 70 < 0 || newY > height - 90)
+        setVelocity({ x: velocity.x, y: -BOUNCE_DAMPING * velocity.y });
+
+      return {
+        x: limitOutput(newX, 50, width - 50),
+        y: limitOutput(newY, 70, height - 90),
+      };
+    });
+    setTime(t);
+  }, [velocity]);
+
+  useEffect(() => {
+    checkGoal();
+  }, [position]);
 
   const checkGoal = () => {
     if (distance(position, goal) < 36) {
       Accelerometer.removeAllListeners();
 
-      let time = 12345;
+      let time = Date.now() - startTime;
       Alert.alert(
         "Nice job!",
         `Your time: ${(time / 1000).toFixed(2)} seconds.`,
         [
-          // {
-          //   text: "Restart",
-          //   onPress: () => {
-          //     console.log(position);
-          //     console.log(startPosition);
-
-          //     // setPosition({ x: 100, y: 100 });
-          //     setPosition(startPosition);
-          //     console.log(position);
-          //     _subscribe();
-          //   },
-          // },
           {
             text: "Go back",
             onPress: () => {
@@ -89,76 +98,40 @@ export default function GameScreen({ navigation }: any) {
     }
   };
 
-  const updatePosition = (event: Coord) => {
-    // p += v * dt
-    // v += a * dt
-    setData(event);
-    setPosition({
-      x: (position.x += -10 * event.x),
-      y: (position.y += 10 * event.y),
-    });
-    checkGoal();
-  };
-
   return (
-    <View style={s.container}>
-      <Pressable onPress={() => alert("lol")}>
-        <Text style={s.text}>
-          x: {position.x.toFixed(2)} y: {position.y.toFixed(2)}
-        </Text>
-      </Pressable>
-      <Text style={s.text}>Accelerometerrr: (in gs where 1g = 9.81 m/s^2)</Text>
-      <Text style={s.text}>x: {x.toFixed(6)}</Text>
-      <Text style={s.text}>y: {y.toFixed(6)}</Text>
-      <Text style={s.text}>z: {z.toFixed(6)}</Text>
-      <View style={s.buttonContainer}>
-        <TouchableOpacity onPress={toggleSubscription} style={s.button}>
-          <Text>{subscription ? "On" : "Off"}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[s.button, s.middleButton]}>
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={s.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View>
-
+    <SafeAreaView style={s.container}>
       <View
-        style={[
-          s.goal,
-          { transform: [{ translateX: goal.x }, { translateY: goal.y }] },
-        ]}
+        style={{
+          position: "absolute",
+          top: 50,
+          left: 30,
+          width: width - 60,
+          height: height - 120,
+          borderWidth: 2,
+        }}
       />
-      {area.map((row, r) =>
-        row.map((cell, c) => (
-          // <Text>cell: {`(${r},${c}): ${cell}`}</Text>
-          <View
-            style={[
-              s.cell,
-              {
-                backgroundColor:
-                  cell === 0
-                    ? "silver"
-                    : cell === 1
-                    ? "lightgreen"
-                    : cell === 2
-                    ? "black"
-                    : "tomato",
-                left: -gridSize / 2,
-                top: -gridSize / 2,
-                width: gridSize,
-                height: gridSize,
-              },
-              {
-                transform: [
-                  { translateX: gridSize * c },
-                  { translateY: gridSize * r },
-                ],
-              },
-            ]}
-          />
-        ))
-      )}
+      <Text>
+        p: ({position.x.toFixed(2)}, {position.y.toFixed(2)})
+      </Text>
+      <Text>
+        v: ({velocity.x.toFixed(2)}, {velocity.y.toFixed(2)})
+      </Text>
+      <Text>
+        w:{width.toFixed(0)}, h:{height.toFixed(0)}
+      </Text>
+      <Pressable
+        onPress={() => {
+          setVelocity({ x: 0, y: 0 });
+          setPosition({ ...startPosition });
+        }}
+      >
+        <Text style={{ fontSize: 24 }}>RESET</Text>
+      </Pressable>
+      <Text style={{ fontSize: 36 }}>Acc. test</Text>
+      <Pressable onPress={handleGyroToggle}>
+        <Text style={{ fontSize: 24 }}>{senorEnabled ? "on" : "off"}</Text>
+      </Pressable>
+
       <View
         style={[
           s.ball,
@@ -166,54 +139,75 @@ export default function GameScreen({ navigation }: any) {
             transform: [{ translateX: position.x }, { translateY: position.y }],
           },
         ]}
-      >
-        <Text style={{ color: "white", fontSize: 10 }}>x{x.toFixed(2)}</Text>
-        <Text style={{ color: "white", fontSize: 10 }}>y{y.toFixed(2)}</Text>
-      </View>
-    </View>
+      />
+      <View
+        style={[
+          s.goal,
+
+          {
+            transform: [{ translateX: goal.x }, { translateY: goal.y }],
+          },
+        ]}
+      />
+    </SafeAreaView>
+    // <View style={s.container}>
+    //   <Pressable onPress={() => alert("lol")}>
+    //     <Text style={s.text}>
+    //       x: {position.x.toFixed(2)} y: {position.y.toFixed(2)}
+    //     </Text>
+    //   </Pressable>
+
+    //  {area.map((row, r) =>
+    //   row.map((cell, c) => (
+    //     // <Text>cell: {`(${r},${c}): ${cell}`}</Text>
+    //     <View
+    //       key={c + "-" + r}
+    //       style={[
+    //         s.cell,
+    //         {
+    //           backgroundColor:
+    //             cell === 0
+    //               ? "silver"
+    //               : cell === 1
+    //               ? "lightgreen"
+    //               : cell === 2
+    //               ? "black"
+    //               : "tomato",
+    //           left: -gridSize / 2,
+    //           top: -gridSize / 2,
+    //           width: gridSize,
+    //           height: gridSize,
+    //         },
+    //         {
+    //           transform: [
+    //             { translateX: gridSize * c + offset },
+    //             { translateY: gridSize * r + offset },
+    //           ],
+    //         },
+    //       ]}
+    //     />
+    //   ))
+    // )}
+    // </View>
   );
 }
 
 const s = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-end",
+    justifyContent: "center",
+    alignItems: "center",
     paddingHorizontal: 20,
     paddingBottom: 20,
   },
-  text: {
-    textAlign: "center",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    alignItems: "stretch",
-    marginTop: 15,
-  },
-  button: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#eee",
-    padding: 10,
-  },
-  middleButton: {
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderColor: "#ccc",
-  },
-
   ball: {
     position: "absolute",
-    backgroundColor: "blue",
+    backgroundColor: "teal",
     borderRadius: 100,
-    // opacity: 0.6,
-    left: -20,
     top: -20,
+    left: -20,
     width: 40,
     height: 40,
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
   goal: {
     position: "absolute",
